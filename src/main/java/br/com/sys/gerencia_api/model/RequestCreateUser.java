2b32ab3796b113252426c4c623da4bebdde92c5d
Amilton <amiltonjose.pe@gmail.com>
2024-06-26 22:33:27 -0300

    refactor: implement oauth2 with jwt and roles more swagger

---
+++ src/main/java/br/com/sys/gerencia_api/model/RequestCreateUser.java
@@ -0,0 +1,4 @@
+package br.com.sys.gerencia_api.model;
+
+public record RequestCreateUser(String username, String email, String password) {
+}
--- src/main/resources/db.migration/V1_create-table-users.sql
+++
@@ -1,7 +0,0 @@
-create table tb_users(
-     id bigint not null auto_increment,
-     nome varchar(100) not null,
-     email varchar(100) not null unique,
-     senha varchar(255) not null,
-     primary key(id)
-)
